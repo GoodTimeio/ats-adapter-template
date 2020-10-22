6aa5f6a7c9f974c6ed84749626556aecda564bc4
yoloVoe <mainali.subrat@utexas.edu>
2020-10-22 16:48:38 -0700

    style: get rid of relative import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule, ValidationPipe } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ApiModule } from '@api/api.module';
 import { CoreModule } from 'src/core/core.module';
 import { RequestLoggerMiddleware } from 'src/core/request-logger.middleware';
 import { RequestRecorderMiddleware } from 'src/core/request-recorder.middleware';
 import { ResponseFieldsInterceptor } from 'src/core/response-fields.interceptor';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { AllExceptionsFilter } from './core/all-exception.filter';
+import { AllExceptionsFilter } from 'src/core/all-exception.filter';
 
 @Module({
     imports: [CoreModule, ApiModule],
